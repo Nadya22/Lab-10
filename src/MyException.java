a75b1698b88c4c7c327025d2006ee422d297d24d
Nadya22 <44775399+Nadya22@users.noreply.github.com>
2019-02-27 16:59:49 +0600

    Update MyException.java

--- src/MyException.java
+++ src/MyException.java
@@ -1,6 +1,25 @@
 import java.util.Date;
 
 public class MyException extends Exception {
+   private int detail;
 
+    MyException(int a) {
+        detail = a;
+    }
+
+    public String toString() {
+        return "MyException" + detail + "]";
+    }
 }
+
+class ExceptoinDemo {
+    static void start3(int a) throws MyException{
+        System.out.println("\nИсключение собственного класса");
+        System.out.println("Названипе метода MyException(" + a + ")");
+        if(a>10)
+            throw new MyException(a);
+        System.out.println("Нормальное завершение");
+    }
+}
+
 
